aff8f37a9810608a4b555e467978385ebf448f18
AleenaCodes <aleena@hotmail.co.uk>
2016-02-27 15:37:00 +0000

    added form

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var fs = require('fs');
+var allTheData = '';
+var querystring = require('querystring');
 
 var server = http.createServer(handler);
 server.listen(3000, function() {
@@ -18,7 +20,7 @@ function handler(request, response){
         console.log(error);
         return;
       }
-
+      console.log('test');
       response.end(file);
     });
   }
@@ -26,7 +28,19 @@ function handler(request, response){
      response.write('girls');
   } else if (endpoint === '/girls') {
      response.write('node?');
-  } else {
+  } else if (endpoint === '/create-post'){
+    response.writeHead(307, {"Location" : "/"});
+    request.on('data', function(chunkOfData){
+      allTheData += chunkOfData;
+    });
+
+    request.on('end', function() {
+      var convertedData = querystring.parse(allTheData);
+      console.log(convertedData);
+      response.end();
+    });
+  }
+  else {
     var extension = endpoint.split('.');
     var ext = extension[1];
     response.writeHead(200, {"Content-Type" : "text/"+ ext});
