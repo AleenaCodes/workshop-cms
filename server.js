57ad2eea8c746d8335f10ec08fbea496ab99a72c
AleenaCodes <aleena@hotmail.co.uk>
2016-02-27 12:13:52 +0000

    tested endpoints

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var http = require('http');
+var f= require('fs');
 
 function handler(request, response){
+  var endpoint = request.url;
+  console.log(endpoint);
+  var method = request.method;
+  console.log(method);
   response.writeHead(200, {"Content-Type" : "text/html"});
-  response.write('message');
+  if (endpoint === '/node') {
+    response.write('girls');
+  } else if (endpoint === '/girls') {
+    response.write('node?');
+  }
   response.end();
 }
 
